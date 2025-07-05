bc26453078614e2a4f1e9cadd8d7d1e3f44fd215
antonver <anton.verbovyi@gmail.com>
2025-07-05 22:00:46 +0300

    Initial commit with CORS support

---
+++ app/main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from app.api import router, admin_router
+
+app = FastAPI()
+
+# Настройка CORS для разрешения запросов с фронтенда
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "https://aeon-messenger.vercel.app",
+        "http://localhost:3000",
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+        "http://127.0.0.1:3000"
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(router)
+app.include_router(admin_router)
