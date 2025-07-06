acdfb56c1692f99a650dcea4b6394792536010fd
antonver <anton.verbovyi@gmail.com>
2025-07-06 16:08:49 +0300

    Fix CORS configuration for Heroku domain

--- backend-hr/app/main.py
+++ backend-hr/app/main.py
@@ -18,7 +18,8 @@ app.add_middleware(
         "http://localhost:3000",
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        "http://127.0.0.1:3000"
+        "http://127.0.0.1:3000",
+        "https://aeon-hr-fixed-backend-540e49434c71.herokuapp.com"
     ],
     allow_credentials=True,
     allow_methods=["*"],
